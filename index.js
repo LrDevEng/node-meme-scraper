79f65a832099ff3c5471cf84679bb56f5f4869fe
LrDevEng <lr.deveng@gmail.com>
2024-09-11 15:14:01 +0200

    Update index.js Receive html string of Website with https get request

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello world.');
+import { get } from 'node:https';
+
+const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+let pageHtml = '';
+
+// Get request to meme website
+get(memeUrl, (res) => {
+  // Set encoding to receive response as string
+  res.setEncoding('utf-8');
+
+  console.log(res.statusCode);
+  console.log(res.headers);
+
+  // Bundle data stream
+  res.on('data', (data) => {
+    pageHtml = pageHtml + data;
+  });
+
+  // Log message when data
+  res.on('end', () => {
+    console.log(pageHtml);
+    console.log(`Received html data from: \n${memeUrl}`);
+  });
+});
