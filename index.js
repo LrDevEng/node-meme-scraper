94189a592997ad52b2677808246bf6c41669b762
LrDevEng <lr.deveng@gmail.com>
2024-09-11 17:01:02 +0200

    Update index.js Add logic to save first image

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+import fs from 'node:fs';
 import { get } from 'node:https';
 import { parse } from 'node-html-parser';
 
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 // Wrapping https get request in promise that resolves once all data has been received
-function asyncHttpsGet(url) {
+function asyncHttpsGet(url, encoding) {
   return new Promise((resolve, reject) => {
-    let stringifiedData = '';
+    let dataBuffer = '';
 
     get(url, (res) => {
-      // Set encoding to receive response as string
-      res.setEncoding('utf-8');
+      // Set encoding
+      res.setEncoding(encoding);
 
       // Bundle incoming data stream
       res.on('data', (data) => {
-        stringifiedData = stringifiedData + data;
+        dataBuffer = dataBuffer + data;
       });
 
       // Resolve when all data has been received and bundled
       res.on('end', () => {
-        resolve(stringifiedData);
+        resolve(dataBuffer);
       });
     }).on('error', (error) => {
       reject(error);
@@ -28,7 +29,7 @@ function asyncHttpsGet(url) {
 }
 
 // Make https get request to meme url and wait until all html data is received
-const htmlPageAsString = await asyncHttpsGet(memeUrl);
+const htmlPageAsString = await asyncHttpsGet(memeUrl, 'utf-8');
 // Parse the html string data into an object
 const htmlRoot = parse(htmlPageAsString);
 
@@ -39,4 +40,16 @@ const allImagesSrc = allImages.map((imgHtmlElement) => {
   return imgHtmlElement.getAttribute('src');
 });
 
-console.log(allImagesSrc);
+const firstImage = await asyncHttpsGet(allImagesSrc[0], 'base64');
+const imageBuffer = Buffer.from(firstImage, 'base64');
+
+console.log(firstImage);
+
+fs.writeFile('xxx_test.jpg', imageBuffer, (error) => {
+  //console.log(error);
+});
+
+// Load the first 10 images and save to memes folder in local file system
+for (let i = 0; i < 10; i++) {}
+
+//console.log(firstImage);
